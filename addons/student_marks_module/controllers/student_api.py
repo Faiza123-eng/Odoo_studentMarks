0c2b57d360c0e9ca4be7100ab5c1014c07e43399
Faiza123-eng <138031890+Faiza123-eng@users.noreply.github.com>
2025-08-12 12:47:08 +0500

    Update student_api.py

--- addons/student_marks_module/controllers/student_api.py
+++ addons/student_marks_module/controllers/student_api.py
@@ -1,29 +1,29 @@
-# from odoo import http
-# from odoo.http import request
-# import json
+from odoo import http
+from odoo.http import request
+import json
 
-# class StudentMarksAPI(http.Controller):
+class StudentMarksAPI(http.Controller):
 
-#     @http.route('/api/student_marks', type='json', auth='user')
-#     def get_all_students(self):
-#         students = request.env['student.marks'].sudo().search([])
-#         return [
-#             {
-#                 'id': student.id,
-#                 'name': student.name,
-#                 'roll_number': student.roll_number,
-#                 'subject': student.subject,
-#                 'marks': student.marks
-#             } for student in students
-#         ]
+    @http.route('/api/student_marks', type='json', auth='user')
+    def get_all_students(self):
+        students = request.env['student.marks'].sudo().search([])
+        return [
+            {
+                'id': student.id,
+                'name': student.name,
+                'roll_number': student.roll_number,
+                'subject': student.subject,
+                'marks': student.marks
+            } for student in students
+        ]
 
-#     @http.route('/api/student_marks/create', type='json', auth='user')
-#     def create_student(self, **kwargs):
-#         data = request.jsonrequest
-#         student = request.env['student.marks'].sudo().create({
-#             'name': data.get('name'),
-#             'roll_number': data.get('roll_number'),
-#             'subject': data.get('subject'),
-#             'marks': data.get('marks'),
-#         })
-#         return {'id': student.id, 'status': 'created'}
+    @http.route('/api/student_marks/create', type='json', auth='user')
+    def create_student(self, **kwargs):
+        data = request.jsonrequest
+        student = request.env['student.marks'].sudo().create({
+            'name': data.get('name'),
+            'roll_number': data.get('roll_number'),
+            'subject': data.get('subject'),
+            'marks': data.get('marks'),
+        })
+        return {'id': student.id, 'status': 'created'}
